ebe28ac9145d0bbd59278e2e462b7f49a14c83e7
lijiahe89757 <243581579@qq.com>
2019-11-24 14:22:28 +0800

    接口的组合

--- retriever/main.go
+++ retriever/main.go
@@ -10,16 +10,44 @@ type Retriever interface {
 	Get(url string) string
 }
 
+type Poster interface {
+	Post(url string, form map[string]string) string
+}
+
+// 接口的组合, RetrieverPoster 里既有 Retriever 又有 Poster
+type RetrieverPoster interface {
+	Retriever
+	Poster
+}
+
 func downLoad(r Retriever)  string {
 	return r.Get("http://www.baidu.com")
 }
 
+func post(poster Poster)  {
+	poster.Post("http://www.baidu.com", map[string]string{
+		"a" : "x",
+		"b" : "y",
+	})
+}
+
+// 既使用了 Poster 接口中的 Post 方法, 又使用了 Retriever 接口中的 Get 方法
+func session(s RetrieverPoster)  string {
+	s.Post("http://www.baidu.com", map[string]string{
+		"contents": "that",
+	})
+	return s.Get("http://www.baidu.com")
+}
+
 func main()  {
 	var r Retriever
-	r = mock.Retriever{Contents:"this"}
+	r = &mock.Retriever{Contents:"this"}
+	fmt.Println(r)
 	r = real.Retriever{
 		UserAgent: "",
 		TimeOut:   0,
 	}
 	fmt.Println(downLoad(r))
+	s := &mock.Retriever{Contents:"this"}
+	fmt.Println(session(s))
 }
--- retriever/mock/retriever.go
+++ retriever/mock/retriever.go
@@ -1,9 +1,28 @@
 package mock
 
+import "fmt"
+
 type Retriever struct {
 	Contents string
 }
 
-func (r Retriever) Get (url string) string {
+// 常用接口 相当于 Java 中的 toString
+func (r *Retriever) String() string {
+	return fmt.Sprintf("Retriver: {Contents=%s}", r.Contents)
+}
+
+/**
+ * 要改变内容必须使用指针接收者
+ * 一致性: 如有指针接收者, 最好都是指针接收者 (这样不会有的是值 有的是指针 容易搞不清楚)
+ */
+
+// 实现 Poster 接口中的 Posts 方法
+func (r *Retriever) Post(url string, form map[string]string) string {
+	r.Contents = form["contents"]
+	return r.Contents
+}
+
+// 实现 Retriever 接口中的 Get 方法
+func (r *Retriever) Get (url string) string {
 	return r.Contents
 }
