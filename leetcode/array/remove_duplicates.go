8d61558ebb8aee55f0de6c1a286d0b40e789d846
lijiahe <243581579@qq.com>
2019-12-12 18:30:10 +0800

    数组去重

--- leetcode/array/remove_duplicates.go
+++ leetcode/array/remove_duplicates.go
@@ -1,10 +1,37 @@
 package main
 
+import "fmt"
+
+// 存在返回 true 否则返回 false
+func check(arr []int, v int) bool {
+	var result bool
+	for _, i := range arr {
+		if i == v {
+			result = true
+			return result
+		} else {
+			result = false
+		}
+	}
+	return result
+}
+
 func removeDuplicates(nums []int) int {
-	return 0
+	j := 0
+	length := len(nums)
+	for i := 0; i < length; i++ {
+		if j == 0 {
+			nums[j] = nums[i]
+			j++
+		} else if !check(nums[:j], nums[i]) {
+			nums[j] = nums[i]
+			j++
+		}
+	}
+	return len(nums[:j])
 }
 
 func main()  {
-	nums := []int{0,0,1,1,1,2,2,3,3,4}
-	removeElement(nums)
+	nums := []int{0,0,1,1}
+	fmt.Println(removeDuplicates(nums))
 }
